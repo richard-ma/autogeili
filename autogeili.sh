54feab07b12769bf0d57f09cdef9369b07fffbc9
Richard Ma <richard.ma.19850509+autogeili@gmail.com>
2011-01-04 06:04:30 +0000

    M    ChangeLog M    autogeili.sh

--- autogeili.sh
+++ autogeili.sh
@@ -12,14 +12,18 @@
 # 	http://www.wordsmotivate.me
 # =============================================================================
 
-readonly DOMAIN_URL=wordsmotivate.me
-readonly IMG_PREFIX_URL=img
-readonly API_PREFIX_URL=api
-readonly IMG_URL=http://$IMG_PREFIX_URL.$DOMAIN_URL/
-readonly API_URL=http://$API_PREFIX_URL.$DOMAIN_URL/
+readonly DATE_TODAY=`date +%Y.%-m.%-d`
+readonly MONTH_TODAY=`date +%Y.%-m`
 readonly WALLPAPER_FILE=today_wallpaper
 readonly CONFIG_FILE=config
 readonly CONFIG_DIR=~/.autogeili
+readonly DOMAIN_URL=wordsmotivate.me
+readonly IMG_PREFIX_URL=img
+readonly API_PREFIX_URL=api
+readonly API_SUFFIX_URL=WallpaperFormat.php
+         IMG_SUFFIX_URL=$MONTH_TODAY/$DATE_TODAY
+         IMG_URL=http://$IMG_PREFIX_URL.$DOMAIN_URL/$IMG_SUFFIX_URL
+         API_URL=http://$API_PREFIX_URL.$DOMAIN_URL/$API_SUFFIX_URL
 
 # 
 # Create CONFIG_DIR if not exsist
@@ -34,16 +38,15 @@ fi
 # 
 # Update date in CONFIG_FILE
 # -----------------------------------------------------------------------------
-today_date=`date +%Y-%d-%m`
 if [ -e $CONFIG_DIR/$CONFIG_FILE ]; then
 	data_date=`cat $CONFIG_DIR/$CONFIG_FILE`
-	if [ $today_date = $data_date ]; then
+	if [ $DATE_TODAY = $data_date ]; then
 		notify-send "Autogeili" "You have already downloaded today's wallpaper." -i /usr/share/pixmaps/gnome-irc.png
 		exit
 	fi
 fi
 
-echo $today_date > $CONFIG_DIR/$CONFIG_FILE
+echo $DATE_TODAY > $CONFIG_DIR/$CONFIG_FILE
 
 # 
 # Setting another wallpaper
@@ -75,6 +78,7 @@ elif  [ `expr $screen_width \* 4` -eq `expr $screen_height \* 5` ]; then
 else
 	success_flg=-1
 fi
+IMG_URL=$IMG_URL\_$resolution
 
 #
 # Remove yesterday wallpaper
@@ -91,7 +95,7 @@ fi
 # Get today wallpaper.
 # -----------------------------------------------------------------------------
 wget \
-	-c $IMG_URL/`date +%Y.%-m`/`date +%Y.%-m.%-d`_$resolution.jpg \
+	-c $IMG_URL.jpg \
        	-O $CONFIG_DIR/$WALLPAPER_FILE.jpg
 
 if [ $? -eq 0 ]; then
@@ -99,7 +103,7 @@ if [ $? -eq 0 ]; then
 	success_flg=0
 else
 	wget \
-		-c $IMG_URL/`date +%Y.%-m`/`date +%Y.%-m.%-d`_$resolution.png \
+		-c $IMG_URL.png \
 	       	-O $CONFIG_DIR/$WALLPAPER_FILE.png
 	if [ $? -eq 0 ]; then
 		suffix=png
