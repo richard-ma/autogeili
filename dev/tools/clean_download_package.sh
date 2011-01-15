0f5cc968d568e9c252c7783323d037b7a0740adb
Richard Ma <richard.ma.19850509+autogeili@gmail.com>
2011-01-15 15:38:28 +0000

    AM   dev/tools/clean_download_package.sh M    dev/tools/create_download_package.sh

--- dev/tools/clean_download_package.sh
+++ dev/tools/clean_download_package.sh
@@ -19,24 +19,3 @@ if [ -e autogeili.tar.gz ]; then
 	rm autogeili.tar.gz
 fi
 
-# 
-# Export codes
-# -----------------
-svn export $ROOT_DIR/ $TMP_DIR
-
-# 
-# Rename TMP_DIR
-# -----------------
-mv $TMP_DIR $RELEASE_DIR
-
-#
-# Remove Dev dirs
-# -----------------
-rm -rf $RELEASE_DIR/dev
-
-# 
-# Create package
-# -----------------
-tar -cf autogeili.tar autogeili/
-gzip autogeili.tar
-
